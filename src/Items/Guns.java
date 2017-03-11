051da92fe1a5eefb9ffe03e66d6f2ec25ee67b11
Codin Iosif Pacuraru <codiniosifpacuraru@Codins-MacBook-Pro.local>
2017-03-11 19:36:39 +0000

    new guns

--- src/Items/Guns.java
+++ src/Items/Guns.java
@@ -6,8 +6,11 @@ package Items;
 public enum Guns {
     NONE (0,0, "no gun"),
     REVOLVER (20, 0.03, "revolver"),
-    KNIFE (25, 0, "knife"),
-    SHOTGUN (50, 0.1, "shotgun");
+    KNIFE (10, 0, "knife"),
+    SHOTGUN (50, 0.1, "shotgun"),
+    LIGHT_PISTOL(10, 0.05, "light pistol"),
+    M4(22, 0.05, "m4"),
+    BIG_IRON(40, 0.02, "big iron");
 
     private final int damage;
     private final double chanceToBrake;
