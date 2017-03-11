b9bdd48b721e209b16b787ddabfda288ef900444
CX-16 <dimarammfire@CX-2.local>
2017-03-11 22:17:07 +0000

    And event

--- src/map/Event.java
+++ src/map/Event.java
@@ -4,4 +4,7 @@ package map;
  * Created by ubuntu on 11/03/17.
  */
 public class Event {
+
+    private Square eventSquare;
+    private TrigerredEvent questTrigerred;
 }
--- src/map/TrigerredEvent.java
+++ src/map/TrigerredEvent.java
@@ -1,7 +1,13 @@
 package map;
 
+import quests.Quest;
+
 /**
  * Created by dimarammfire on 11.03.17.
  */
 public class TrigerredEvent {
+
+    private Quest questTrigger;
+
+
 }
